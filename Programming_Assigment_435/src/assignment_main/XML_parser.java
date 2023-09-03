16eb41667a034274d8d3ecee8e6665747cbe081a
ackolar <ackolar@wm.edu>
2023-09-02 20:12:56 -0400

    Corrected child node parsing

--- Programming_Assigment_435/src/assignment_main/XML_parser.java
+++ Programming_Assigment_435/src/assignment_main/XML_parser.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -40,8 +42,8 @@ public class XML_parser {
 		
 		
 		switch(actionCode) {
-		case 0:
-		default: highlightUI();
+			case 0:
+			default: highlightUI();
 		}
 		
 		
@@ -49,30 +51,58 @@ public class XML_parser {
 	}
 	
 	
-	private void getLeaves() {
+	private ArrayList<Element> getLeaves() {
 		
 		ArrayList<Element> leaves = new ArrayList<>();
-		Element docElement = doc.getDocumentElement();
-	    NodeList nList = docElement.getChildNodes();
-	    
+		
+		
+		
+		// adapted from this post https://stackoverflow.com/questions/20783506/get-leaf-nodes-xml-parsing-in-java
+		try {
+			  
+			  final XPathExpression xpath = XPathFactory.newInstance().newXPath().compile("//*[count(./*) = 0]");
+			  final NodeList nodeList = (NodeList) xpath.evaluate(doc, XPathConstants.NODESET);
+			  System.out.println(nodeList.getLength());
+			  for(int i = 0; i < nodeList.getLength(); i++) {
+			    final Element el = (Element) nodeList.item(i);
+			    leaves.add(el);
+			  }
+			} catch (Exception e) {
+			  e.printStackTrace();
+			}
+		
+		
+		/*NodeList nList = doc.getElementsByTagName("node");
 	    int length = nList.getLength();
+	    System.out.println(length);
 	    for (int i = 0; i < length; i++) {
-	        if (!nList.item(i).hasChildNodes()) {
-	            Element leaf = (Element) nList.item(i);
+	        
+	    	System.out.println(nList.item(i));
+	    	if (!nList.item(i).hasChildNodes()) {
+	            
+	        	Element leaf = (Element) nList.item(i);
 	            leaves.add(leaf);       
 	        }
-	    }
+	    }*/
 		
 		for (Element l: leaves) {
-			System.out.println(l.toString());
+			System.out.println(l.getAttribute("text"));
 		}
+		
+		return leaves;
 		
 	
 	}
 	
 	private void highlightUI() {
 		
-		getLeaves();		
+		ArrayList<Element> leaves = new ArrayList<>();
+		leaves = getLeaves();
+		
+		for (Element l: leaves) {
+			l.setAttribute("border", "1");
+		}
+		
 	}
 	
 	
